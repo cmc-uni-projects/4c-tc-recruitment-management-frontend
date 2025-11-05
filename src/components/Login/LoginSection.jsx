fee811206ba863e3a97b6400482338467b91c896
Linh. Pham Khanh - CMC Global TTS <pklinh3@cmcglobal.vn>
2025-11-05 16:08:22 +0700

    delete yup bị thừa

--- src/components/Login/LoginSection.jsx
+++ src/components/Login/LoginSection.jsx
@@ -4,7 +4,6 @@ import LoginSocial from "./LoginSocial";
 import * as Yup from "yup";
 import { login } from "../../services/auth.services";   // ✅ Import login API
 import { useNavigate } from "react-router-dom";         // ✅ Điều hướng
- import * as Yup from 'yup';
  import bannerImg from '../../assets/logo.jpg';
  
 
