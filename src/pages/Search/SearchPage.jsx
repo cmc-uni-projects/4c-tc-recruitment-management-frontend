409cedc7d74ce90294be25e34bd1057c5e9d0ff6
khanhlinhoabinh <khanhlinhphamhb@gmail.com>
2025-11-11 23:18:50 +0700

    trang tim kiem job

---
+++ src/pages/Search/SearchPage.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import SearchSection from "../../components/Home/Search/SearchSection";
+import Navbar from "../../components/Layout/Navbar";
+
+export default function SearchPage() {
+  return (
+    <div>
+    <Navbar />
+      <SearchSection />
+    </div>
+  );
+}
