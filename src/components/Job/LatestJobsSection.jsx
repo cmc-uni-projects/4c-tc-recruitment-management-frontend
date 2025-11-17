173baf9f3b7c8524edeef20f399ed9e7d97f913a
Huy. Nguyen Tien - CMC Global TTS <nthuy6@cmcglobal.vn>
2025-11-17 16:55:50 +0700

    sua conflict

--- src/components/Job/LatestJobsSection.jsx
+++ src/components/Job/LatestJobsSection.jsx
@@ -7,7 +7,7 @@ import axios from "axios";
 import { toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
-toast.configure();
+
 
 export default function LatestJobsSection() {
   const [latestJobs, setLatestJobs] = useState([]);
