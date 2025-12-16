1856f1273fce4489452c4379edb8fd6743cba4cf
Huy. Nguyen Tien - CMC Global TTS <nthuy6@cmcglobal.vn>
2025-12-16 13:50:35 +0700

    up code setting hr

---
+++ src/pages/HR/HRSettingPage.jsx
@@ -0,0 +1,14 @@
+
+import React from "react";
+import HRSection from "../../components/HR/HRSection"; // Layout đang dùng ở trang mẫu
+import HRSetting from "../../components/HR/HRSetting";
+
+function HRSettingPage() {
+  return (
+    <HRSection title="Cài Đặt">
+      <HRSetting />
+    </HRSection>
+  );
+}
+
+export default HRSettingPage;
